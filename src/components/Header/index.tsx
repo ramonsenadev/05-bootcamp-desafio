2c6f4e3bf5d7d4878fc9b271f5756393bcaf3bd6
Ramon Sena <ramonsenadev@gmail.com>
2021-04-08 08:50:47 -0300

    Definição do componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
