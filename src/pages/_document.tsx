d2b384dfa9d3cfb14a83746bbc45fa5853e67a53
Ramon Sena <ramonsenadev@gmail.com>
2021-04-11 23:04:58 -0300

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
+
+          <link rel="icon" type="image/png" href="/favicon.png" />
         </Head>
         <body>
           <Main />
